88c159cc8344c486ffe6339170183266889cfec3
chadjaa_sofiane <sofianne012@gmail.com>
2023-07-22 14:34:20 +0100

    change storybook config to handle svg and ts paths

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,18 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import tsPathsWebpackPlugin from "tsconfig-paths-webpack-plugin";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "storybook-addon-turbo-build",
+      options: {
+        optimizationLevel: 2,
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -12,6 +20,27 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  async webpackFinal(config) {
+    config.resolve?.plugins?.push(new tsPathsWebpackPlugin());
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+    config.module?.rules?.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg?url$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i
+    return config;
   },
 };
 export default config;
